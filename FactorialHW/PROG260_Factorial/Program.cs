5875e0aa88c9ce14e27f161d1912a50438d79738
lazarom37 <55485519+lazarom37@users.noreply.github.com>
2022-10-20 19:48:43 -0700

    starting recursive hw

---
+++ FactorialHW/PROG260_Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PROG260_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer between 1-10: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(factor(n));
+
+        }
+
+        public static int factor(int n)
+        {
+            if (n > 0)
+            {
+                return n * factor(n - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
+
+//Created by Marcus Lazaro, Fall 2022
---
+++ RecurRevStringProb/RecurRevString/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurRevString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string to check it is palindrome or not : ");
+            Console.WriteLine("Enter the word    exit     to exit the program.");
+            string word = Console.ReadLine();
+            word = word.ToLower(); // force to lower case
+          
+            while (word != "exit")
+            {
+                if (word == RecStingRevPub(word))  // compare the input to the string returned from the method
+                {
+                    Console.WriteLine("{0} is a Palindrome string\n", word);
+                }
+                else
+	            {
+                    Console.WriteLine("{0} is not a Palindrome string\n", word);
+                }
+
+                // do it again
+                Console.WriteLine("Enter string to check it is palindrome or not : ");
+                Console.WriteLine("Enter the word exit to exit the program.");
+                word = Console.ReadLine();
+                word = word.ToLower(); // force to lower case
+            }
+
+            Console.WriteLine("Goodbye.");
+            Console.ReadLine();
+        }
+
+        //===================================================================================================
+        // write code for this method that takes in a word and returns that word after reversing the order of the letters
+        // You algorium MUST use recursion.  
+
+        //  method takes in original word and returns the result of a series of recursive calls
+        public static string RecStingRevPub(string input) 
+        {
+            // write code here that calls itself (RecStingRevPub) over and over
+            // and eventually returns the input word reversed, 
+            // if input was "party" it would return to the call in Main  "ytrap"
+            // I would expect your code to deal with 1 letter of the word in each cycle through
+            // the recurion, either on the way down, or the way back out.
+
+        }
+
+    }
+}
